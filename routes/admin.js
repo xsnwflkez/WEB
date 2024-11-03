85a1494319bf131dafe6dbc943b8ac84ad704f1e
xsnwflkez <lemontea2582@gmail.com>
2024-11-03 18:55:06 +0700

    zzz

--- routes/admin.js
+++ routes/admin.js
@@ -15,7 +15,7 @@ const adminBro = new AdminBro({
   databases: [mongoose],
   rootPath: "/admin",
   branding: {
-    companyName: "BestBags",
+    companyName: "Best",
     logo: "/images/shop-icon.png",
     softwareBrothers: false,
   },
